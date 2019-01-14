1eb4e2638da995e7043271f2b0c6d90b2b78e677
LexFTW <lexmengual@gmail.com>
2019-01-14 20:24:22 +0100

    14/01/2019

---
+++ app/src/main/java/com/example/tnb_20/pilotarebotant/Bola.java
@@ -0,0 +1,68 @@
+package com.example.tnb_20.pilotarebotant;
+
+import android.widget.ImageView;
+
+public class Bola {
+
+    private ImageView ball;
+    private int width, height, x, y;
+    private double displacementX, displacementY;
+
+    public Bola(){}
+
+    public ImageView getBall() {
+        return ball;
+    }
+
+    public void setBall(ImageView ball){
+        this.ball = ball;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width){
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height){
+        this.height = height;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+
+    public double getDisplacementX() {
+        return displacementX;
+    }
+
+    public double getDisplacementY() {
+        return displacementY;
+    }
+
+    public void setDisplacementX(double displacementX){
+        this.displacementX = displacementX;
+    }
+
+    public void setDisplacementY(double displacementY){
+        this.displacementY = displacementY;
+    }
+}
--- app/src/main/java/com/example/tnb_20/pilotarebotant/MainActivity.java
+++ app/src/main/java/com/example/tnb_20/pilotarebotant/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -17,13 +18,17 @@ public class MainActivity extends AppCompatActivity {
     private double displacementX;
     private double displacementY;
     private int x, y;
+    // Atributos correctos.
     private int status;
     private DisplayMetrics displayMetrics; // Serveix per obtenir les dimensions de la pantalla.
+    private ArrayList<Bola> bolas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.displayMetrics = this.getBaseContext().getResources().getDisplayMetrics();
+        this.status = getResources().getDimensionPixelSize(getResources().getIdentifier("status_bar_height", "dimen", "android"));
 
         createWindow();
 
@@ -39,58 +44,82 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void moveBall() {
-        double positionX = this.ball.getX() + (this.x * this.displacementX);
-        double positionY = this.ball.getY() + (this.y * this.displacementY);
+        for (Bola bola: bolas) {
+            double positionX = bola.getBall().getX() + (bola.getX() * bola.getDisplacementX());
+            double positionY = bola.getBall().getY() + (bola.getY() * bola.getDisplacementY());
 
-        checkPositionX(positionX);
-        checkPositionY(positionY);
+            checkPositionX(bola, positionX);
+            checkPositionY(bola, positionY);
+
+            intersection();
+        }
     }
 
-    private void checkPositionY(double positionY) {
-        if(this.ball.getY() > 0){
-            if(positionY + this.ball.getHeight() + this.status < this.height){
-                this.ball.setY((float) positionY);
+    private void intersection(){
+        // Si 0a >= 0b || 0a <= 0b + width{}
+        if(bolas.get(0).getBall().getX() >= bolas.get(1).getBall().getX() || bolas.get(0).getBall().getX() <= bolas.get(1).getBall().getX() + bolas.get(1).getBall().getWidth()){
+            bolas.get(0).setDisplacementX(bolas.get(0).getDisplacementX() * (-1));
+            bolas.get(1).setDisplacementX(bolas.get(1).getDisplacementX() * (-1));
+        }
+    }
+
+    private void checkPositionX(Bola bola, double positionX){
+        if(bola.getBall().getX() > 0){
+            if(positionX + bola.getBall().getWidth() < bola.getWidth()){
+                bola.getBall().setX((float) positionX);
             }else{
-                this.ball.setY((float) height - this.ball.getHeight() - this.status);
-                this.displacementY = this.displacementY * (-1);
+                bola.getBall().setX((float) bola.getWidth() - bola.getBall().getWidth());
+                bola.setDisplacementX(bola.getDisplacementX() * (-1));
             }
         }else{
-            if (positionY > 0) {
-                this.ball.setY((float) positionY);
+            if (positionX > 0) {
+                bola.getBall().setX((float) positionX);
             } else{
-                this.ball.setY(0);
-                this.displacementY = this.displacementY * (-1);
+                bola.getBall().setX(0);
+                bola.setDisplacementX(bola.getDisplacementX() * (-1));
             }
         }
     }
 
-    private void checkPositionX(double positionX) {
-        if(this.ball.getX() > 0){
-            if(positionX + this.ball.getWidth() < this.width){
-                this.ball.setX((float) positionX);
-            }else{
-                this.ball.setX((float) width - this.ball.getWidth());
-                this.displacementX = this.displacementX * (-1);
+    private void checkPositionY(Bola bola, double positionY) {
+        if (bola.getBall().getY() > 0) {
+            if (positionY + bola.getBall().getHeight() + this.status < bola.getHeight()) {
+                bola.getBall().setY((float) positionY);
+            } else {
+                bola.getBall().setY((float) bola.getHeight() - bola.getBall().getHeight() - this.status);
+                bola.setDisplacementY(bola.getDisplacementY() * (-1));
             }
-        }else{
-            if (positionX > 0) {
-                this.ball.setX((float) positionX);
-            } else{
-                this.ball.setX(0);
-                this.displacementX = this.displacementX * (-1);
+        } else {
+            if (positionY > 0) {
+                bola.getBall().setY((float) positionY);
+            } else {
+                bola.getBall().setY(0);
+                bola.setDisplacementY(bola.getDisplacementY() * (-1));
             }
         }
     }
 
     private void createWindow() {
-        this.displacementX = 4.0;
-        this.displacementY = 4.0;
-        this.x = 2;
-        this.y = 2;
-        this.ball = (ImageView) findViewById(R.id.ball);
-        this.displayMetrics = this.getBaseContext().getResources().getDisplayMetrics();
-        this.width = this.displayMetrics.widthPixels;
-        this.height = this.displayMetrics.heightPixels;
-        this.status = getResources().getDimensionPixelSize(getResources().getIdentifier("status_bar_height", "dimen", "android"));
+        this.bolas = new ArrayList<>();
+        Bola bola = new Bola();
+        bola.setDisplacementY(20.0);
+        bola.setDisplacementX(20.0);
+        bola.setX(4);
+        bola.setY(4);
+        bola.setBall((ImageView) findViewById(R.id.ball));
+        bola.setWidth(this.displayMetrics.widthPixels);
+        bola.setHeight(this.displayMetrics.heightPixels);
+        this.bolas.add(bola);
+
+        Bola bola2 = new Bola();
+        bola2.setDisplacementY(8.0);
+        bola2.setDisplacementX(8.0);
+        bola2.setX(2);
+        bola2.setY(2);
+        bola2.setBall((ImageView) findViewById(R.id.ball2));
+        bola2.setWidth(this.displayMetrics.widthPixels);
+        bola2.setHeight(this.displayMetrics.heightPixels);
+
+        this.bolas.add(bola2);
     }
 }
